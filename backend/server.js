c5bc4df50971f75f2c7f35eb6fd98a719a78c824
Esteban <nunezesteban750@gmail.com>
2025-11-30 19:00:03 -0300

    endpoint para obtener pedidos

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { connectDB } from './database/db.js';
 import { Product } from './models/ProductSchema.js';
 import ProductRoutes from './routes/ProductsRoute.js'
 import UsersRoutes from './routes/UsersRoute.js'
+import PedidosRoutes from './routes/PedidosRoute.js'
 
 dotenv.config();
 const app = express();
@@ -21,12 +22,13 @@ app.use('/api/productos', ProductRoutes)
 
 app.use('/api/auth', UsersRoutes)
 
+app.use('/api/pedidos', PedidosRoutes)
+
 await connectDB();
 // Rutas
 app.get('/', (req, res) => {
   res.send('API de la Mueblería Hermanos Jota');
 });
-
 
 
 
